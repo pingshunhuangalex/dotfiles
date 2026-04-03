71197bf0835ee9b075471923ec70f3590c63fa7f
Pingshun Huang <alex.g.huang@gmail.com>
2026-04-03 23:43:46 +1100

    Use path variables instead of hard-coded paths

--- dot_config/private_fish/conf.d/002_interactive.fish
+++ dot_config/private_fish/conf.d/002_interactive.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-    set -l interactive_files ~/.config/fish/conf.d/interactive/*.fish
+    set -l interactive_files $XDG_CONFIG_HOME/fish/conf.d/interactive/*.fish
 
     if set -q interactive_files[1]
         for file in $interactive_files

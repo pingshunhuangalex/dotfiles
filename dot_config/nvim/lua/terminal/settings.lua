8d10e05cacf053c5e819ec70e7c12a481d6d13f3
Pingshun Huang <alex.g.huang@gmail.com>
2026-05-05 22:13:10 +1000

    Add cSpell configs support

--- dot_config/nvim/lua/terminal/settings.lua
+++ dot_config/nvim/lua/terminal/settings.lua
@@ -1,3 +1,4 @@
 local opt = vim.opt
 
 opt.inccommand = "split" -- Preview substitutions live in a split panel
+opt.showcmd = true       -- Display partially entered commands and info in the status section

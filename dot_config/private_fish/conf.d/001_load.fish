db6b5f2aa945e8664276bef1728cbc9d78ac68a3
Pingshun Huang <alex.g.huang@gmail.com>
2026-06-26 15:49:54 +1000

    Add QMK configs support

--- dot_config/private_fish/conf.d/001_load.fish
+++ dot_config/private_fish/conf.d/001_load.fish
@@ -2,3 +2,8 @@
 if not set -q HOMEBREW_PREFIX; and test -x /opt/homebrew/bin/brew
     /opt/homebrew/bin/brew shellenv fish | source
 end
+
+# UV
+if test -f $XDG_DATA_HOME/../bin/env.fish
+    source $XDG_DATA_HOME/../bin/env.fish
+end
